ea32848a9aebf5d320d7327cf3beffa46ca0217b
mhuseynn <hmehdiyev2005@gmail.com>
2024-02-14 20:07:10 +0400

    t

---
+++ LessonDapper/Entities/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace LessonDapper.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Id_Author { get; set; }
+    public int YearPress { get; set; }
+
+    public int Pages { get; set; }
+ 
+}
